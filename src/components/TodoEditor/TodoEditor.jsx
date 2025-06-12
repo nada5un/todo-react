a325415c2f6a7785eceb1f3e802f88c77335f416
daeun <sung980524@naver.com>
2025-06-12 22:47:53 +0900

    modify : add Todo onKeyDown

--- src/components/TodoEditor/TodoEditor.jsx
+++ src/components/TodoEditor/TodoEditor.jsx
@@ -9,22 +9,32 @@ const TodoEditor = ({ onSubmit }) => {
         setText(e.target.value);
     };
 
+    const onClickAddTodo = () => {
+        inputRef.current.focus();
+        if (text.trim() === "") {
+            return;
+        }
+        onSubmit(text);
+        setText("");
+    };
+
     return (
         <div className="TodoEditor">
             <h3>새로운 Todo 작성하기 ✏️</h3>
             <div className="TodoEditor__wrapper">
-                <input ref={inputRef} value={text} onChange={handleChange} type="text" placeholder="새로운 Todo..." />
-                <button
-                    onClick={() => {
-                        inputRef.current.focus();
-                        if (text.trim() === "") {
-                            return;
+                <input
+                    ref={inputRef}
+                    value={text}
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="새로운 Todo..."
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            onClickAddTodo();
                         }
-                        onSubmit(text);
-                        setText("");
                     }}
-                    type="button"
-                >
+                />
+                <button onClick={onClickAddTodo} type="button">
                     추가
                 </button>
             </div>
